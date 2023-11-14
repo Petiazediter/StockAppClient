3f64cbb58d87fb1a71b3c6b0104f57aecd7f6943
Peti <49650243+Petiazediter@users.noreply.github.com>
2023-11-14 21:29:13 +0100

    feat: move search and quicksearch logic to searchForm.tsx

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,19 +1,44 @@
 'use client'
+import { ChangeEvent, FormEvent, useState } from "react"
 
-import { FormEvent } from "react"
-const SearchForm = () => {
+interface Props {
+    onKeywordChange?: (keyword: string) => void
+    onQuickSearchKeywordChange?: (keyword: string) => void
+}
+
+const SearchForm = (props: Props) => {
+
+    const [quickOptionsTimeout, setQuickOptionsTimeout] = useState<NodeJS.Timeout | undefined>()
 
     const handleSearch = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('search button clicked! :)')
+        const formData = new FormData(e.currentTarget)
+        const formValues = Object.fromEntries(formData) as { keyword: string };
+        props.onKeywordChange && props.onKeywordChange(formValues.keyword)
     }
-    
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if ( quickOptionsTimeout ) {
+            clearTimeout(quickOptionsTimeout)
+        }
+        setQuickOptionsTimeout(setTimeout( () => {
+            handleQuickOptions(e.target.value)
+        }, 300 ))
+    }
+
+    const handleQuickOptions = (keyword: string) => {
+        console.log('Quick options for keyword: ', keyword)
+    }
+
     return (
         <form 
             onSubmit={handleSearch}
             id='search-form'
             className='flex gap-2 flex-col md:flex-row'>
             <input 
+                onChange={handleInputChange}
+                name="keyword"
+                required
                 className="rounded-md px-4 py-2"
                 type="search" placeholder="Search for a stock by name or symbol" />
             <button 
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+"use client"
+import { useState } from "react";
 import SearchForm from "./components/SearchForm";
 
 export default function Home() {
+
+  const [keyword, setKeyword] = useState('')
+  const [quickSearchKeyword, setQuickSearchKeyword] = useState<string | undefined>()
+
   return (
     <main className="m-5">
       <h1 className='text-3xl m-4'>Hello Search view!</h1>
-      <SearchForm />
+      <SearchForm 
+        onKeywordChange={setKeyword}
+        onQuickSearchKeywordChange={setQuickSearchKeyword}
+        />
     </main>  
   )
 }
