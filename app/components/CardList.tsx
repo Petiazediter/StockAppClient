3bcc41e4350fcc62c880febe3042bcf7c2dcbf01
Peti <49650243+Petiazediter@users.noreply.github.com>
2023-11-16 13:20:17 +0100

    refactor: implement loading into cardList

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -1,19 +1,34 @@
-interface Props {
-    data: {key: string, value: any}[]
+import LoadingBlock from "./LoadingBlock"
+
+type Props = {
+    isLoading?: boolean
+    data?: {key: string, value: any}[]
     itemClassName?: string
     containerClassName?: string
 }
 
 const CardList = (props: Props) => {
+    if ( (!props.data && !props.isLoading) || (props.data && props.data.length === 0) ) {
+        return 'No results!'
+    }
+
     return (
-    <ul 
-        className={`grid grid-cols-1 md:grid-cols-3 gap-2 ${props.containerClassName}`}>
-        { props.data.map( (item) => (
-            <li
-                className={`shadow-md my-2 p-5 rounded-md ${props.itemClassName}`}
-                key={item.key}>{item.value}</li>
-        ))}
-    </ul>)  
+        <ul 
+            className={`grid grid-cols-1 md:grid-cols-3 gap-2 ${props.containerClassName}`}>
+            { 
+                props.isLoading ?
+                    Array.from(new Array(5)).map((v) => (
+                        <LoadingBlock key={v} />
+                    ))
+                : props.data!.map( (item) => (
+                    <li
+                        className={`shadow-md my-2 p-5 rounded-md ${props.itemClassName}`}
+                        key={item.key}>{item.value}
+                    </li>
+                )
+            )}
+        </ul>
+    )  
 }
 
 export default CardList
--- app/components/StockList.tsx
+++ app/components/StockList.tsx
@@ -1,21 +1,13 @@
-import Link from "next/link"
 import { StockData } from "../hooks/useClientAPICall"
-import LoadingBlock from "./LoadingBlock"
 import StockItem from "./StockItem"
 import CardList from "./CardList"
 
 interface Props {
     stocks?: StockData[]
     isLoading: boolean
-    loadingBlocks?: number
 }
 
-const StockList = ({ stocks, isLoading, loadingBlocks = 5 }: Props) => {
-
-    if ( isLoading ) {
-        return Array.from(new Array(5)).map((v) => (<LoadingBlock key={v} />))
-    }
-
+const StockList = ({ stocks, isLoading }: Props) => {
     if ( !stocks ) {
         return <h2>Search for a stock by name or symbol</h2>
     }
@@ -23,21 +15,11 @@ const StockList = ({ stocks, isLoading, loadingBlocks = 5 }: Props) => {
     if ( stocks && stocks.length === 0 ) {
         return <h2>No results!</h2>
     }
-    
-    /*<ul className=' grid grid-cols-1 md:grid-cols-3 gap-2'>
-            { stocks.map( (stock, i) => {
-                    return (
-                        <Link href={`/stock/${stock['1. symbol']}`} key={`stock-list-item-${stock['1. symbol']}`}>
-                            <StockItem stock={stock} />
-                        </Link>
-                    )
-                })
-            }
-        </ul> */
 
     return (
       
         <CardList 
+            isLoading={isLoading}
             data={stocks.map( (stock) => (
                 {
                     key: stock['1. symbol'],
